dc20da9956c2a7447d0e7b306f4b44c2f3311dec
paradigma23! <katacyri@mail.ru>
2023-04-08 15:15:31 +0300

    to intensive

--- src/common/Chose.tsx
+++ src/common/Chose.tsx
@@ -5,6 +5,7 @@ import { images } from "../assets/img";
 import { useAppDispatch, useAppSelector } from "../redux/state";
 import { coursesState } from "../redux/changePage";
 import { filteredState, getFilteredCourse } from "../redux/filterCourceSlice";
+import { Link } from "react-router-dom";
 // компонент на странице определенного курса
 
 const Chose: React.FC<any> = (props) => {
@@ -45,54 +46,56 @@ const Chose: React.FC<any> = (props) => {
             .map((el: IFilterContent, i: number) => (
               // filterCources.filteredData.map(
               //   (el: { [key: string]: string }, i: number) => (
-              <article
-                key={i}
-                className="flex flex-col lg:flex-row lg:pr-[25px] gap-0 sm:gap-[20px] items-center border border-solid border-[#272523] rounded-[30px]"
-              >
-                <div
-                  className={`inline-flex relative min-h-full w-full lg:w-fit justify-center items-center py-10 px-5 rounded-[29px] 
-                  ${el.color === "orange" ? "bg-[#FC9F7D]" : ""}
-                  ${el.color === "blue" ? "bg-[#7BCDD0]" : ""}
-                  ${el.color === "violet" ? "bg-[#DEC3E4]" : ""}
-                  `}
+              <Link to="/intensive">
+                <article
+                  key={i}
+                  className="flex flex-col lg:flex-row lg:pr-[25px] gap-0 sm:gap-[20px] items-center border border-solid border-[#272523] rounded-[30px]"
                 >
-                  <p className="w-[234px]  h-fit text-[24px] sm:text-[34px] font-semibold">
-                    {el.deadline}
-                    <img
-                      className="absolute right-[20px] sm:right-0 bottom-0 w-[100px] h-[100px] sm:w-[145px] sm:h-[145px]"
-                      src={images.oSTar}
-                      alt=""
-                    />
-                  </p>
-                </div>
-                <div className="flex flex-col sm:flex-row gap-[15px] sm:gap-[20px] p-[15px] sm:p-[25px] lg:p-0">
-                  <div>
-                    <h3 className="text-[18px] font-semibold mb-[10px]">
-                      {el.desc}
-                    </h3>
-                    <p className="text-[12px] sm:text-[14px]">{el.text}</p>
+                  <div
+                    className={`inline-flex relative min-h-full w-full lg:w-fit justify-center items-center py-10 px-5 rounded-[29px] 
+                ${el.color === "orange" ? "bg-[#FC9F7D]" : ""}
+                ${el.color === "blue" ? "bg-[#7BCDD0]" : ""}
+                ${el.color === "violet" ? "bg-[#DEC3E4]" : ""}
+                `}
+                  >
+                    <p className="w-[234px]  h-fit text-[24px] sm:text-[34px] font-semibold">
+                      {el.deadline}
+                      <img
+                        className="absolute right-[20px] sm:right-0 bottom-0 w-[100px] h-[100px] sm:w-[145px] sm:h-[145px]"
+                        src={images.oSTar}
+                        alt=""
+                      />
+                    </p>
                   </div>
-                  <span className="w-full h-[1px] sm:w-[1px] sm:h-auto bg-[#272523] z-10  block"></span>
-                  <div className="flex flex-row sm:flex-col justify-between sm:justify-start">
-                    <div className="flex flex-col">
-                      <span className="text-[12px] sm:text-[14px]">
-                        Продолжительность
-                      </span>
-                      <span className="inline-block mb-[12px] text-[18px] font-medium sm:font-semibold">
-                        {el.certainDur}
-                      </span>
+                  <div className="flex flex-col sm:flex-row gap-[15px] sm:gap-[20px] p-[15px] sm:p-[25px] lg:p-0">
+                    <div>
+                      <h3 className="text-[18px] font-semibold mb-[10px]">
+                        {el.desc}
+                      </h3>
+                      <p className="text-[12px] sm:text-[14px]">{el.text}</p>
                     </div>
-                    <div className="flex flex-col">
-                      <span className="text-[12px] sm:text-[14px]">
-                        Стоимость
-                      </span>
-                      <span className=" text-[18px] font-medium sm:font-semibold">
-                        {el.numCoast}
-                      </span>
+                    <span className="w-full h-[1px] sm:w-[1px] sm:h-auto bg-[#272523] z-10  block"></span>
+                    <div className="flex flex-row sm:flex-col justify-between sm:justify-start">
+                      <div className="flex flex-col">
+                        <span className="text-[12px] sm:text-[14px]">
+                          Продолжительность
+                        </span>
+                        <span className="inline-block mb-[12px] text-[18px] font-medium sm:font-semibold">
+                          {el.certainDur}
+                        </span>
+                      </div>
+                      <div className="flex flex-col">
+                        <span className="text-[12px] sm:text-[14px]">
+                          Стоимость
+                        </span>
+                        <span className=" text-[18px] font-medium sm:font-semibold">
+                          {el.numCoast}
+                        </span>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </article>
+                </article>
+              </Link>
             ))}
         </div>
       </div>
