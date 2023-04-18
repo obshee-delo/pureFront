eef29d730ac5f761fdf8793d74088b3a99951734
ensi <66754841+ncor@users.noreply.github.com>
2023-04-18 10:03:06 +0300

    fix: added client authorization

--- src/sdk/sdk.ts
+++ src/sdk/sdk.ts
@@ -1,17 +1,18 @@
+import 'dotenv/config';
 import { Sdk } from "obsheedelo-sdk/dist/sdk";
 import {
   ApiAdapter,
   Connection,
   Credentials,
 } from "obsheedelo-sdk/dist/adapter";
-// ИНИЦИАЛИЗАЦИЯ
+
 export const sdk = new Sdk(
   new ApiAdapter(
     { transport: "http", host: "0.0.0.0", port: 5432 } as Connection,
-    { token: "" } as Credentials
+    { token: process.env.ADMIN_JWT } as Credentials
   )
 );
-// ТИП КУРСА
+
 export type Course = {
   id: string;
   name: string;
